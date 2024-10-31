b935bf9a55374c318412324829da10e370bb8ae1
jpelehwany <jpelehwany@loyola.edu>
2024-10-31 12:51:36 -0400

    Added beginning of main.py (names, lab number, program purpose, etc)

---
+++ main.py
@@ -0,0 +1,6 @@
+# John Elehwany and Owen Sanchez
+# Loyola CompSci 151
+# Professor Zee
+# Lab: 08
+
+# This program simulates rolling a six-sided die a number of times dependent on the user, and then it displays the results of the number of rolls.
