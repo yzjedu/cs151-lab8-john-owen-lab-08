ea0a2a1cc9a439ab6a72d65cfb2f71fe7c9c0153
jpelehwany <jpelehwany@loyola.edu>
2024-10-31 13:31:26 -0400

    Added some test code to main.py

--- main.py
+++ main.py
@@ -4,3 +4,17 @@
 # Lab: 08
 
 # This program simulates rolling a pair of six-sided dice a number of times dependent on the user, and then it displays the results of the number of rolls.
+
+import random
+
+def roll():
+    dice1 = random.randint(1, 6)
+    dice2 = random.randint(1, 6)
+    return dice1, dice2
+
+num_rolls = int(input("How many times would you like to roll? "))
+
+for i in range(num_rolls):
+    dice1, dice2 = roll()
+    total = dice1 + dice2
+    print(f'You rolled {dice1} and {dice2}.` Your total is {total}')
