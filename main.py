767ef389e9a1a6911d8e64769c4a066d034e47df
jpelehwany <jpelehwany@loyola.edu>
2024-10-31 12:52:43 -0400

    Added beginning of main.py (names, lab number, program purpose, etc)

--- main.py
+++ main.py
@@ -3,4 +3,4 @@
 # Professor Zee
 # Lab: 08
 
-# This program simulates rolling a six-sided die a number of times dependent on the user, and then it displays the results of the number of rolls.
+# This program simulates rolling a pair of six-sided dice a number of times dependent on the user, and then it displays the results of the number of rolls.
